bdb82f0558c083d357665a901eb3ec88456a82d1
Matt Thoburn <mthoburn96@gmail.com>
2017-06-30 06:23:30 -0400

    front end done

--- app/app.py
+++ app/app.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+order = list()
+
 @app.route("/main")
 def main():
     return render_template("index.html")
 
 @app.route("/cart")
 def cart():
-    order = [("Pizza","Exra Cheese",7),("Coke","Not the Beverage",2),("Cookie","extra cookie",1)]
+    #order = [("Pizza","Exra Cheese",7),("Coke","Not the Beverage",2),("Cookie","extra cookie",1)]
     price = 0
     for tup in order:
         price += tup[2]
@@ -16,6 +18,15 @@ def cart():
 @app.route("/submit")
 def submit():
     return render_template("submit.html")
+
+@app.route("/addToCart")
+def addToCart():
+    item = request.args.get("item")
+    comment = request.args.get("comment")
+    price = request.args.get("price")
+    order.append((str(item),str(comment),float(price)))
+    print order
+    return render_template("index.html")
 if __name__ == "__main__":
     app.run()
 
